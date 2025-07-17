3aecc95b7c4ace1d1c5b28829aae90f0f0a6eb49
julien bouland <bouland.julien@gmail.com>
2025-07-17 22:54:08 +0200

    :tada: Initialize airtest gem

---
+++ lib/air_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "air_test/version"
+
+module AirTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/air_test/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AirTest
+  VERSION = "0.1.0"
+end
