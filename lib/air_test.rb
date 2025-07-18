bf3c542837837a0b506c997170e3c0c19b013dc7
julien bouland <bouland.julien@gmail.com>
2025-07-18 18:40:42 +0200

    :green_heart: Fix wrong naming in configuration file

--- lib/air_test.rb
+++ lib/air_test.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "air_test/version"
-require_relative "air_test_automation/configuration"
-require_relative "air_test_automation/notion_parser"
-require_relative "air_test_automation/spec_generator"
-require_relative "air_test_automation/github_client"
-require_relative "air_test_automation/rake_tasks"
+require_relative "air_test/configuration"
+require_relative "air_test/notion_parser"
+require_relative "air_test/spec_generator"
+require_relative "air_test/github_client"
+require_relative "air_test/rake_tasks"
 
 module AirTest
   class << self
