8ec8eb6da8741a36f293cdb03d0361ee0c2064fe
julien bouland <bouland.julien@gmail.com>
2025-07-18 18:43:33 +0200

    :green_heart: Remove useless require in configuration file

--- lib/air_test.rb
+++ lib/air_test.rb
@@ -5,8 +5,6 @@ require_relative "air_test/configuration"
 require_relative "air_test/notion_parser"
 require_relative "air_test/spec_generator"
 require_relative "air_test/github_client"
-require_relative "air_test/rake_tasks"
-
 module AirTest
   class << self
     attr_accessor :configuration
