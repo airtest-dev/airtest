b1bfa605767b2457623e7e7f013bec34c224c542
julien bouland <bouland.julien@gmail.com>
2025-07-20 18:06:06 +0200

    Fix Rails Engine path for Rake tasks

--- lib/air_test/engine.rb
+++ lib/air_test/engine.rb
@@ -4,7 +4,7 @@ module AirTest
   class Engine < ::Rails::Engine
     # This ensures that the Rake tasks are automatically loaded when the gem is included in a Rails app
     rake_tasks do
-      load File.expand_path("tasks/air_test.rake", __dir__)
+      load File.expand_path("../../tasks/air_test.rake", __FILE__)
     end
   end
 end
