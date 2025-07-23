c9a1a49ca87425356b806447a51b384971ed3bba
julien bouland <bouland.julien@gmail.com>
2025-07-23 19:47:11 +0200

    :sparkles: Add jira integration

--- lib/air_test/jira_ticket_parser.rb
+++ lib/air_test/jira_ticket_parser.rb
@@ -1,35 +1,68 @@
 # frozen_string_literal: true
 
-require_relative "ticket_parser"
+require 'net/http'
+require 'json'
+require 'uri'
+require_relative 'ticket_parser'
 
 module AirTest
   class JiraTicketParser
     include TicketParser
     def initialize(config = AirTest.configuration)
-      # Add Jira-specific config here
+      @domain = config.jira_domain || 'https://boulandjulien.atlassian.net'
+      @api_key = config.jira_token
+      @project_key = config.jira_project_id || 'SCRUM'
+      @email = config.jira_email || ENV['JIRA_EMAIL']
     end
 
     def fetch_tickets(limit: 5)
-      # TODO: Implement Jira API fetch logic
-      []
+      jql = "project = #{@project_key} AND status = 'To Do' ORDER BY created DESC"
+      uri = URI("#{@domain}/rest/api/3/search?jql=#{URI.encode_www_form_component(jql)}&maxResults=#{limit}")
+      request = Net::HTTP::Get.new(uri)
+      request.basic_auth(@email, @api_key)
+      request['Accept'] = 'application/json'
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      response = http.request(request)
+      return [] unless response.code == '200'
+      data = JSON.parse(response.body)
+      data['issues']
     end
 
-    def parse_ticket_content(page_id)
-      # TODO: Implement Jira ticket parsing
-      nil
+    def parse_ticket_content(issue_id)
+      # Fetch issue details (description, etc.)
+      uri = URI("#{@domain}/rest/api/3/issue/#{issue_id}")
+      request = Net::HTTP::Get.new(uri)
+      request.basic_auth(@email, @api_key)
+      request['Accept'] = 'application/json'
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      response = http.request(request)
+      return nil unless response.code == '200'
+      issue = JSON.parse(response.body)
+      # Example: parse description as feature, steps, etc. (customize as needed)
+      {
+        feature: issue.dig('fields', 'summary') || '',
+        scenarios: [
+          {
+            title: 'Scenario',
+            steps: [issue.dig('fields', 'description', 'content')&.map { |c| c['content']&.map { |t| t['text'] }&.join(' ') }&.join(' ') || '']
+          }
+        ],
+        meta: { tags: [], priority: '', estimate: nil, assignee: (issue.dig('fields', 'assignee', 'displayName') || '') }
+      }
     end
 
     def extract_ticket_title(ticket)
-      # TODO: Map Jira fields to unified fields
-      "No title"
+      ticket.dig('fields', 'summary') || 'No title'
     end
 
     def extract_ticket_id(ticket)
-      "No ID"
+      ticket['key'] || 'No ID'
     end
 
     def extract_ticket_url(ticket)
-      ""
+      "#{@domain}/browse/#{ticket['key']}"
     end
   end
 end 
