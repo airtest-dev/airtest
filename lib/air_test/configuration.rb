f72e100ba6b9e3f6848662a97254977f5ad1972f
julien bouland <bouland.julien@gmail.com>
2025-07-26 12:31:09 +0200

    :wrench: Update configuration file for better developer UX

--- lib/air_test/configuration.rb
+++ lib/air_test/configuration.rb
@@ -4,21 +4,46 @@
 module AirTest
   # Handles configuration for AirTest, including API tokens and environment variables.
   class Configuration
-    attr_accessor :notion_token, :notion_database_id, :jira_token, :jira_project_id, :jira_domain, :jira_email, :monday_token, :monday_board_id, :monday_domain, :github_token, :repo, :tool
+    attr_accessor :tool, :notion, :jira, :monday, :github, :repo
 
     def initialize
       @tool = ENV.fetch("AIRTEST_TOOL", "notion")
-      @notion_token = ENV.fetch("NOTION_TOKEN", nil)
-      @notion_database_id = ENV.fetch("NOTION_DATABASE_ID", nil)
-      @jira_token = ENV.fetch("JIRA_TOKEN", nil)
-      @jira_project_id = ENV.fetch("JIRA_PROJECT_ID", nil)
-      @jira_domain = ENV.fetch("JIRA_DOMAIN", nil)
-      @jira_email = ENV.fetch("JIRA_EMAIL", nil)
-      @monday_token = ENV.fetch("MONDAY_TOKEN", nil)
-      @monday_board_id = ENV.fetch("MONDAY_BOARD_ID", nil)
-      @monday_domain = ENV.fetch("MONDAY_DOMAIN", nil)
-      @github_token = ENV["GITHUB_BOT_TOKEN"] || ENV.fetch("GITHUB_TOKEN", nil)
-      @repo = ENV.fetch("REPO", nil)
+      @notion = {
+        token: ENV["NOTION_TOKEN"],
+        database_id: ENV["NOTION_DATABASE_ID"]
+      }
+      @jira = {
+        token: ENV["JIRA_TOKEN"],
+        project_id: ENV["JIRA_PROJECT_ID"],
+        domain: ENV["JIRA_DOMAIN"],
+        email: ENV["JIRA_EMAIL"]
+      }
+      @monday = {
+        token: ENV["MONDAY_TOKEN"],
+        board_id: ENV["MONDAY_BOARD_ID"],
+        domain: ENV["MONDAY_DOMAIN"]
+      }
+      @github = {
+        token: ENV["GITHUB_BOT_TOKEN"] || ENV["GITHUB_TOKEN"]
+      }
+      @repo = ENV["REPO"]
+    end
+
+    def validate!
+      case tool.to_s.downcase
+      when "notion"
+        raise "Missing NOTION_TOKEN" unless notion[:token]
+        raise "Missing NOTION_DATABASE_ID" unless notion[:database_id]
+      when "jira"
+        raise "Missing JIRA_TOKEN" unless jira[:token]
+        raise "Missing JIRA_PROJECT_ID" unless jira[:project_id]
+        raise "Missing JIRA_DOMAIN" unless jira[:domain]
+        raise "Missing JIRA_EMAIL" unless jira[:email]
+      when "monday"
+        raise "Missing MONDAY_TOKEN" unless monday[:token]
+        raise "Missing MONDAY_BOARD_ID" unless monday[:board_id]
+        raise "Missing MONDAY_DOMAIN" unless monday[:domain]
+      end
     end
   end
 end
