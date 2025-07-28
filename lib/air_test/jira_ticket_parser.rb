b47f0989b4691556f0662c4bfe7afcc539bb4b70
julien bouland <bouland.julien@gmail.com>
2025-07-28 13:33:35 +0200

    :rocket: Fix issues with tool connexion

--- lib/air_test/jira_ticket_parser.rb
+++ lib/air_test/jira_ticket_parser.rb
@@ -12,21 +12,33 @@ module AirTest
       @domain = config.jira[:domain]
       @api_key = config.jira[:token]
       @project_key = config.jira[:project_id]
-      @email = config.jira[:email] || ENV['JIRA_EMAIL']
+      @email = config.jira[:email]
     end
 
     def fetch_tickets(limit: 5)
-      jql = "project = #{@project_key} AND status = 'To Do' ORDER BY created DESC"
-      uri = URI("#{@domain}/rest/api/3/search?jql=#{URI.encode_www_form_component(jql)}&maxResults=#{limit}")
-      request = Net::HTTP::Get.new(uri)
-      request.basic_auth(@email, @api_key)
-      request['Accept'] = 'application/json'
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      response = http.request(request)
-      return [] unless response.code == '200'
-      data = JSON.parse(response.body)
-      data['issues']
+      # Try different status names (English and French)
+      statuses = ['To Do', 'À faire', 'Open', 'New']
+      all_issues = []
+      
+      statuses.each do |status|
+        jql = "project = #{@project_key} AND status = '#{status}' ORDER BY created DESC"
+        uri = URI("#{@domain}/rest/api/3/search?jql=#{URI.encode_www_form_component(jql)}&maxResults=#{limit}")
+        request = Net::HTTP::Get.new(uri)
+        request.basic_auth(@email, @api_key)
+        request['Accept'] = 'application/json'
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        response = http.request(request)
+        
+        if response.code == '200'
+          data = JSON.parse(response.body)
+          issues = data['issues'] || []
+          all_issues.concat(issues)
+          puts "Found #{issues.length} issues with status '#{status}'" if issues.any?
+        end
+      end
+      
+      all_issues.first(limit)
     end
 
     def parse_ticket_content(issue_id)
